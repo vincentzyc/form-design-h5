2c90d91f6c07bfb8057ab3a6255f5406e3e55f19
Vincentyun <vincentzyc@qq.com>
2019-04-23 15:53:19 +0800

    样式优化

--- src/components/widget-items/index.vue
+++ src/components/widget-items/index.vue
@@ -20,7 +20,7 @@
 
         <div v-if="item.type === 'imgSlide'" class="wg-imgslide">
           <div :style="{margin:item.style.margin}">
-            <div :style="{width:'100%',height:item.style.height+'px'}">
+            <div :style="{width:'100%',height:item.style.height/88.88+'rem'}">
               <cube-slide :ref="item.key" :data="item.value" :interval="item.interval" style="max-width:640px">
                 <cube-slide-item v-for="(images, index) in item.value" :key="index">
                   <a :href="images.url">
