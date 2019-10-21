e2b12fef03c1e74b5f0fa14c18f62c9af4c33321
Vincentyun <vincentzyc@qq.com>
2019-10-21 16:25:36 +0800

    文本描述新增背景颜色

--- src/components/widget-items/wg-statictext.vue
+++ src/components/widget-items/wg-statictext.vue
@@ -2,7 +2,7 @@
   <div
     class="wg-staticText clearfix"
     @click="jumpH5(item.link)"
-    :style="{backgroundImage:`url(${item.backgroundImage||''})`}"
+    :style="{backgroundColor:item.backgroundColor,backgroundImage:`url(${item.backgroundImage||''})`}"
   >
     <p :style="$util.formatStyle(item.style)" v-html="$util.textBr(item.value)"></p>
   </div>
