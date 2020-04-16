653cd68d7608f4f441970edf4283ec2640f881e7
Vincentyun <vincentzyc@qq.com>
2020-04-16 13:54:18 +0800

    add vant

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  "plugins": [
+    [
+      "import",
+      {
+        "libraryName": "vant",
+        "libraryDirectory": "es",
+        "style": true
+      },
+      "vant"
+    ]
   ]
 }
