51377d44f13b086a19543aa32ba1bb8c2106f716
Vincentyun <vincentzyc@qq.com>
2020-05-03 10:36:35 +0800

    代码优化

--- src/components/render-page.vue
+++ src/components/render-page.vue
@@ -25,7 +25,7 @@
 
 <script>
 import WidgetItems from "@/components/widget-items";
-import { valiDate, submit } from "@/assets/js/validate"
+
 export default {
   name: "renderPage",
   components: {
@@ -37,40 +37,10 @@ export default {
   },
   data() {
     return {
-      formData: {},
-      showCode: false,
       showFixedBottom:true
     };
   },
   methods: {
-    async handleSubmit() {
-      let valiDateRes = this.valiAllDate(this.BUS.pageData.list);
-      if (valiDateRes !== true) return this.$createToast({ txt: valiDateRes, type: 'txt', time: 2000 }).show()
-      submit(this.formData);
-    },
-    valiAllDate(list) {
-      for (const item of list) {
-        if (item.type === "formList") {
-          let res = this.valiAllDate(item.list);
-          if (res !== true) return res;
-        }
-        if (!item.apiKey) continue;
-        let res = valiDate(item);
-        if (res === true) {
-          this.formatParam(item);
-          continue
-        }
-        return res;
-      }
-      return true;
-    },
-    formatParam(item) {
-      if (!item.hasOwnProperty("apiKey")) return;
-      if (item.type === 'phone' && item.showCode) {
-        this.formData[item.codeKey] = item.codeValue;
-      }
-      this.formData[item.apiKey] = item.value;
-    },
     showFixed() {
       if (this.fixedBottom.length > 0) {
         this.showFixedBottom = this.fixedBottom[0].scrollHeight === 0;
@@ -79,21 +49,9 @@ export default {
           this.showFixedBottom = scrollTop >= this.fixedBottom[0].scrollHeight
         })
       }
-    },
-    busEvent() {
-      this.BUS.$on('handleClick', data => {
-        switch (data.type) {
-          case 'submit':
-            this.handleSubmit()
-            break;
-          default:
-            break;
-        }
-      });
     }
   },
   created() {
-    this.busEvent();
     this.showFixed()
   }
 }
