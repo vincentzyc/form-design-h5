278c21a0510b42b7caea6d01b516ef2aefd23f23
Vincentyun <vincentzyc@qq.com>
2020-05-24 13:35:33 +0800

    新增跑马灯组件

--- src/components/widget-items/index.vue
+++ src/components/widget-items/index.vue
@@ -1,35 +1,6 @@
 <template>
   <div class="widget-view" :key="item.key">
-    <!-- 手机 -->
-    <WgPhone v-if="item.type === 'phone'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 输入框 -->
-    <WgInput v-if="item.type === 'input'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 选择框 -->
-    <WgCheckbox v-if="item.type === 'checkbox'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 下拉选择 -->
-    <WgSelect v-if="item.type === 'select'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 开关 -->
-    <WgSwitch v-if="item.type === 'switch'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 日期选择 -->
-    <WgDate v-if="item.type === 'date'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 横向滑动自动选择 -->
-    <WgHorizontalPicker v-if="item.type === 'hPicker'" :ref="item.key" :item="item" class="wg-padding" />
-    <!-- 图片展示 -->
-    <WgImgshow v-if="item.type === 'imgShow'" :item="item" />
-    <!-- 图片轮播 -->
-    <wgImgSlide v-if="item.type === 'imgSlide'" :item="item" class="wg-padding" />
-    <!-- 按钮 -->
-    <WgButton v-if="item.type === 'button'" :item="item" />
-    <!-- 文本描述 -->
-    <WgStaticText v-if="item.type === 'staticText'" :item="item" />
-    <!-- 分割线 -->
-    <wgSplitLine v-if="item.type === 'splitLine'" :item="item" class="wg-padding" />
-    <!-- 微信关注 -->
-    <WgWechat v-if="item.type === 'wechat'" :item="item" />
-    <!-- 跑马灯 -->
-    <WgMarquee v-if="item.type === 'marquee'" :item="item" />
-    <!-- 用户协议 -->
-    <WgAgreement v-if="item.type === 'agreement'" :item="item" class="wg-padding" />
+    <component :is="wgNameMap[item.type]" :item="item" />
   </div>
 </template>
 
@@ -49,7 +20,7 @@ import WgHorizontalPicker from "./wg-horizontal-picker"
 import WgWechat from './wg-wechat'
 import WgMarquee from './wg-marquee'
 import WgAgreement from './wg-agreement'
-
+import WgMarqueeSingle from './wg-marquee-single'
 
 export default {
   name: "widgetItems",
@@ -68,12 +39,35 @@ export default {
     WgHorizontalPicker,
     WgWechat,
     WgMarquee,
-    WgAgreement
+    WgAgreement,
+    WgMarqueeSingle
   },
   props: {
     item: {
       type: Object,
       required: true
+    }
+  },
+  data() {
+    return {
+      wgNameMap: {
+        phone: 'WgPhone',
+        input: 'WgInput',
+        checkbox: 'WgCheckbox',
+        select: 'WgSelect',
+        switch: 'WgSwitch',
+        date: 'WgDate',
+        button: 'WgButton',
+        splitLine: 'wgSplitLine',
+        staticText: 'WgStaticText',
+        imgshow: 'WgImgshow',
+        imgSlide: 'wgImgSlide',
+        hPicker: 'WgHorizontalPicker',
+        wechat: 'WgWechat',
+        marquee: 'WgMarquee',
+        agreement: 'WgAgreement',
+        marqueeSingle: 'WgMarqueeSingle'
+      }
     }
   }
 };
