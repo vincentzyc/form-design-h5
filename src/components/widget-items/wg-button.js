859b44e216049e647d8e132540d6a8df908dcc07
Vincentyun <vincentzyc@qq.com>
2020-06-25 10:23:48 +0800

    jsx

---
+++ src/components/widget-items/wg-button.js
@@ -0,0 +1,38 @@
+import { handleSubmit } from "../../validate"
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    clickBtn(item) {
+      switch (item.btnType) {
+        case 'submit':
+          handleSubmit()
+          break;
+        default:
+          break;
+      }
+    }
+  },
+  render() {
+    const { item } = this;
+    const children = item.style.isImgBtn ?
+      <img src={item.style.value} alt="图片按钮" width="100%" onClick={() => this.clickBtn(item)} />
+      : (
+        <button
+          class="wg-button"
+          style={this.$util.formatStyle(item.style.btnStyle)}
+          onClick={() => this.clickBtn(item)}
+        >{item.btnText}</button>
+      )
+    return (
+      <div class="flex flex-center" style={this.$util.formatStyle(item.style)}>
+        {children}
+      </div >
+    )
+  }
+}
+
