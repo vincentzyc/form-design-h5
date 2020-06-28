e90a4568537d78ce573c8695c2934db2415a0195
Vincentyun <vincentzyc@qq.com>
2020-06-28 15:05:30 +0800

    functional component

---
+++ src/components/widget-items/wg-video-play.js
@@ -0,0 +1,18 @@
+import Utils from '../../utils'
+export default {
+  functional: true,
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  render(h, context) {
+    const { item } = context.props;
+    return (
+      <div class="wg-video-play" style={Utils.formatStyle(item.style)}>
+        <video attrs={item.videoAttr} style="width:100%;height:100%">您的浏览器不支持视频播放哦~</video>
+      </div>
+    )
+  }
+}
