6dd221054df439107b46bb0fe37937ce8db8ce04
Vincentyun <vincentzyc@qq.com>
2020-07-02 12:48:31 +0800

    代码优化

--- src/components/widget-items/wg-statictext.js
+++ src/components/widget-items/wg-statictext.js
@@ -1,11 +1,5 @@
 import Utils from '../../utils'
 
-function jumpH5(link) {
-  let isLink = Utils.isLink(link);
-  if (!isLink) return;
-  window.location.href = link
-}
-
 export default {
   functional: true,
   props: {
@@ -23,7 +17,7 @@ export default {
     return (
       <div
         class="wg-staticText clearfix"
-        onClick={() => jumpH5(item.link)}
+        onClick={() => Utils.jumpLink(item.link)}
         style={domStyle}
       >
         <p style={Utils.formatStyle(item.style)} domPropsInnerHTML={Utils.changeRem(item.value)}></p>
