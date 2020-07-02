a2e906c5b1635cefc8afffb8f9169cc561f23107
Vincentyun <vincentzyc@qq.com>
2020-07-02 12:48:20 +0800

    代码优化

--- src/components/widget-items/wg-button.js
+++ src/components/widget-items/wg-button.js
@@ -21,13 +21,11 @@ export default {
     const { item } = this;
     const children = item.style.isImgBtn ?
       <img src={item.style.value} alt="图片按钮" width="100%" onClick={() => this.clickBtn(item)} />
-      : (
-        <button
-          class="wg-button"
-          style={this.$util.formatStyle(item.style.btnStyle)}
-          onClick={() => this.clickBtn(item)}
-        >{item.btnText}</button>
-      )
+      : <button
+        class="wg-button"
+        style={this.$util.formatStyle(item.style.btnStyle)}
+        onClick={() => this.clickBtn(item)}
+      >{item.btnText}</button>
     return (
       <div class="flex flex-center" style={this.$util.formatStyle(item.style)}>
         {children}
