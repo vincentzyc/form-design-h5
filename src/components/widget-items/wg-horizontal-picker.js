58be67a71886289493ee88532c7e3fc604f4d19a
Vincentyun <vincentzyc@qq.com>
2020-07-06 11:40:14 +0800

    jsx

---
+++ src/components/widget-items/wg-horizontal-picker.js
@@ -0,0 +1,34 @@
+import Utils from '@/utils/index'
+import HorizontalPicker from "@/components/base/horizontal-picker"
+
+export default {
+  // components: {
+  //   'horizontal-picker': HorizontalPicker
+  // },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  render() {
+    const { item } = this;
+    const wrapClass = ['wg-item', item.label.labelPosition === 'top' ? 'flex-column' : 'align-middle'],
+      titleStyle = { width: Utils.changeRem(item.label.labelwidth) }
+    return (
+      <div id={item.key} class={wrapClass} style={item.style}>
+        <div class="wg-title" style={titleStyle}>{item.label.labelTitle}</div>
+        <div class="flex-auto" id={item.key}>
+          <HorizontalPicker
+            vModel={item.value}
+            style="margin: 0 auto;"
+            data={item.options}
+            pickerStyle={item.pickerStyle}
+            itemWidth={item.itemWidth}
+            showNumber={item.showNumber}
+          ></HorizontalPicker>
+        </div>
+      </div>
+    )
+  }
+}
