6021dd9f084480dd47ac6bd682c3d1cfac52e1ab
Vincentyun <vincentzyc@qq.com>
2020-07-09 10:27:48 +0800

    jsx

---
+++ src/components/widget-items/wg-wechat.js
@@ -0,0 +1,71 @@
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    copy() {
+      this.$copyText(this.item.wechatName).then(() => {
+        this.$createDialog({
+          type: 'alert',
+          title: '复制成功',
+          content: '请在微信粘贴搜索',
+          icon: 'cubeic-right',
+          onConfirm: () => this.confirm()
+        }).show()
+      }, () => {
+        this.$createDialog({
+          type: 'alert',
+          title: '很抱歉',
+          content: '您的浏览器不支持自动复制公众号，请手动复制后打开微信搜索'
+        }).show()
+      })
+    },
+    confirm() {
+      if (this.$util.isWechat()) return;
+      window.location.href = "weixin://"
+    },
+    getImgBtn(item) {
+      return (
+        <div class="flex flex-center" onClick={() => this.copy()} style={this.$util.formatStyle(item.style)}>
+          <img src={item.style.value} alt="图片按钮" width="100%" />
+        </div>
+      )
+    },
+    getWechat(item) {
+      return (
+        <div class="flex align-middle wg-wechat-wrap" style={this.$util.formatStyle(item.style)}>
+          {this.getLogo(item)}
+          {this.getWechatName(item)}
+          <div
+            class={['wechat-btn', item.showName ? 'flex-none' : 'flex-auto']}
+            style={this.$util.formatStyle(item.style.btnStyle)}
+            onClick={() => this.copy()}
+          >{item.btnText}</div>
+        </div>
+      )
+    },
+    getLogo(item) {
+      return item.showLogo && (
+        <div class="wechat-avatar flex-none">
+          {item.logo
+            ? <img src={item.logo} alt="公众号logo" width="100%" />
+            : <img src={require('@/assets/img/wxlogo.png')} alt="公众号logo" width="100%" />}
+        </div>
+      )
+    },
+    getWechatName(item) {
+      return item.showName && <div class="flex-auto">{item.value}</div>
+    }
+  },
+  render() {
+    const { item } = this
+    return (
+      <div class="wg-wechat clearfix" style={{ backgroundColor: item.backgroundColor }}>
+        {item.style.isImgBtn ? this.getImgBtn(item) : this.getWechat(item)}
+      </div>
+    )
+  }
+}
