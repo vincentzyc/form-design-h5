c5d5e7834d42e4ec23c02bd1ad5939fbbea63eea
Vincentyun <vincentzyc@qq.com>
2020-07-14 09:57:02 +0800

    代码优化

--- src/components/widget-items/wg-agreement/index.js
+++ src/components/widget-items/wg-agreement/index.js
@@ -2,10 +2,6 @@ import Utils from '@/utils/index'
 import { Checkbox } from 'vant';
 import AgreementPopup from './agreement-popup.vue'
 export default {
-  components: {
-    "agreement-popup": AgreementPopup,
-    "van-checkbox": Checkbox
-  },
   props: {
     item: {
       type: Object,
@@ -19,7 +15,7 @@ export default {
     },
     getAgreementList(h, item) {
       return (
-        <van-checkbox vModel={item.value} style={Utils.formatStyle(item.style)} shape="square">
+        <Checkbox vModel={item.value} style={Utils.formatStyle(item.style)} shape="square">
           <p style={{ color: item.style.color }}>
             <span>{item.tipText}</span>
             {item.titleTexts.map((titleText, key) => (
@@ -34,7 +30,7 @@ export default {
               </span>
             ))}
           </p>
-        </van-checkbox>
+        </Checkbox>
       )
     }
   },
@@ -43,7 +39,7 @@ export default {
     return (
       <div class="wg-agreement clearfix" id={item.key}>
         {this.getAgreementList(h, item)}
-        <agreement-popup ref="agreement" />
+        <AgreementPopup ref="agreement" />
       </div>
     )
   }
