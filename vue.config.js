7eab533bca0a1a2245de685ae7be308b9bc98c7c
Vincentyun <vincentzyc@qq.com>
2020-11-07 10:05:40 +0800

    🔧build: vue use cdn

--- vue.config.js
+++ vue.config.js
@@ -2,28 +2,36 @@ module.exports = {
   publicPath: "./",
   outputDir: "docs",
   productionSourceMap: false,
-  
+
+  configureWebpack: {
+    // 以下库使用cdn，不会被打包
+    externals: process.env.NODE_ENV === 'production' ? {
+      "vue": "Vue",
+    } : {}
+  },
+
   chainWebpack: config => {
+    config.plugins.delete('preload')
     config.plugins.delete('prefetch')
   },
 
-	devServer: {
-		port: 9010,
-	},
-	css: {
-		loaderOptions: {
-			stylus: {
-				'resolve url': true,
-				'import': [
-					'./src/theme'
-				]
-			}
-		}
-	},
-	pluginOptions: {
-		'cube-ui': {
-			postCompile: true,
-			theme: true
-		}
-	}
+  devServer: {
+    port: 9010,
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  }
 }
