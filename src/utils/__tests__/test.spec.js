8c6a17594f4f64212dfe74713525201bc2885cc8
vincentzyc <vincentzyc@qq.com>
2022-09-18 14:46:06 +0800

    🧪 test: jest test

--- src/utils/__tests__/test.spec.js
+++ src/utils/__tests__/test.spec.js
@@ -1,4 +1,5 @@
 import utils from "../index";
+import { later } from "@/tests"
 
 test('getType', () => {
   const obj = {}, arr = [], num = 1, str = "1", bool = false, isNull = null, isUndefined = undefined;
@@ -10,3 +11,19 @@ test('getType', () => {
   expect(utils.getType(isNull)).toBe('Null');
   expect(utils.getType(isUndefined)).toBe('Undefined');
 })
+
+test("setCookie getCookie", () => {
+  const mockId = "cookie";
+  const mockData = "cookie data";
+  utils.setCookie(mockId, mockData, 2);
+  expect(utils.getCookie(mockId)).toEqual(mockData);
+});
+
+test("delCookie", async () => {
+  const mockId = "cookie";
+  const mockData = "cookie data";
+  utils.setCookie(mockId, mockData, 1);
+  await later()
+  utils.delCookie(mockId)
+  expect(utils.getCookie(mockId)).toEqual('');
+});
