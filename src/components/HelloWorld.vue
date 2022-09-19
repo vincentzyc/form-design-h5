04abca60df224beba50b2729e24a63b48d65388d
vincentzyc <vincentzyc@qq.com>
2022-09-19 09:08:49 +0800

    🧪 test: jest test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <span class="count">{{ count }}</span>
+    <button @click="increment">Increment</button>
   </div>
 </template>
 
@@ -9,6 +11,16 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
   },
 };
 </script>
