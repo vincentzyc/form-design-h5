0fb21b736913e6e0cb8b3d71414b6b425b110dce
vincentzyc <vincentzyc@qq.com>
2022-10-17 11:17:58 +0800

    ✨ feat: 新增滑块校验组件

---
+++ src/components/widget-items/wg-drag-verify.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <DragVerify
+      ref="dragVerify"
+      :isPassing.sync="item.value"
+      :text="item.placeholder"
+      :successText="item.successText"
+      handlerIcon="el-icon-d-arrow-right"
+      successIcon="el-icon-circle-check"
+    />
+  </div>
+</template>
+
+<script>
+import DragVerify from "@/components/base/DragVerify.vue";
+export default {
+  components: {
+    DragVerify,
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
