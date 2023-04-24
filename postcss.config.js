2a5c64d28e6c77cdca7ddf406037f55352a82520
vincentzyc <vincentzyc@qq.com>
2023-04-24 12:20:35 +0800

    🐳chore: 更新依赖

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,20 @@
 //rem自动转px，根据实际情况决定是否使用
+// module.exports = {
+//   plugins: {
+//     autoprefixer: {},
+//     'postcss-px2rem': {
+//       remUnit: 50
+//     }
+//   }
+// }
+
+// const autoprefixer = require('autoprefixer');
+const px2rem = require('postcss-pxtorem');
+ 
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-px2rem': {
-      remUnit: 50
-    }
-  }
-}
+  plugins: [
+  // autoprefixer(),
+  px2rem({
+    rootValue: 50
+  })], 
+};
