777ee5d01e0adb26c1610c9751abe02dc7810011
vincentzyc <vincentzyc@qq.com>
2023-04-26 11:09:11 +0800

    🐳chore: 更新依赖

--- src/components/base/horizontal-picker.vue
+++ src/components/base/horizontal-picker.vue
@@ -1,178 +1,178 @@
-
 <template>
-  <Scroll
-    ref="scroll"
-    direction="horizontal"
-    class="horizontal-scroll-list-wrap"
-    :style="{ width: itemWidth*showNumber + 'px'}"
-    :options="BScollOptions"
-    v-if="data.length>0"
-  >
-    <div ref="wrapper" class="picker-slot-wrapper" :style="{width: (data.length+showNumber-1) * itemWidth + 'px'}">
-      <div class="picker-item" :style="{width: itemWidth * (showNumber - 1) / 2 + 'px'}"></div>
-      <div
-        class="picker-item"
-        v-for="(item,key) in data"
-        :key="item"
-        :style="[{width:itemWidth + 'px'},item === currentValue ? pickerStyle : {}]"
-        @click="handClick(key)"
-      >
-        <span v-if="!pickerSlot">{{item}}</span>
-        <slot v-else :item="item"></slot>
+  <div class="horizontal-container" ref="wgWrapper" :style="{ width: itemWidth * showNumber + 'px' }">
+    <div class="scroll-wrapper">
+      <div class="scroll-content" :style="{ width: (data.length + showNumber - 1) * itemWidth + 'px' }">
+        <div class="scroll-item" :style="{ width: (itemWidth * (showNumber - 1)) / 2 + 'px' }"></div>
+        <div
+          class="scroll-item"
+          v-for="(item, key) in data"
+          :key="key"
+          :style="activeItem(item)"
+          @click="handClick(key)"
+        >
+          <template v-if="pickerSlot"><slot></slot></template>
+          <span v-else>{{ item }}</span>
+        </div>
+        <div class="scroll-item" :style="{ width: (itemWidth * (showNumber - 1)) / 2 + 'px' }"></div>
       </div>
-      <div class="picker-item" :style="{width: itemWidth * (showNumber - 1) / 2 + 'px'}"></div>
     </div>
-  </Scroll>
+  </div>
 </template>
 
-<script type="text/ecmascript-6">
-import { Scroll } from 'cube-ui'
+<script>
+import { computed, defineComponent, nextTick, onMounted, ref } from 'vue';
+import BScroll from '@better-scroll/core';
 
 const ITEM_WIDTH = 48;
 const SHOW_NUMBER = 5;
 
-export default {
-  name: "HorizontalPicker",
-  components: {
-    Scroll
-  },
+export default defineComponent({
+  name: 'HorizontalPicker',
   props: {
-    value: {
+    modelValue: {
       type: [String, Number],
       default: '',
     },
     pickerSlot: {
       type: Boolean,
-      default: false
+      default: false,
     },
     pickerStyle: {
       type: Object,
       default: () => {
         return {
-          color: '#d68317'
-        }
-      }
+          color: '#d68317',
+        };
+      },
     },
     itemWidth: {
       type: Number,
-      default: ITEM_WIDTH
+      default: ITEM_WIDTH,
     },
     showNumber: {
       type: Number,
-      default: SHOW_NUMBER
+      default: SHOW_NUMBER,
     },
     data: {
       type: Array,
-      required: true
+      required: true,
     },
     defaultIndex: {
       type: Number,
-      default: 0
-    }
-  },
-  data() {
-    return {
-      currentIndex: 0,
-      BScoll: null,
-      BScollOptions: {
-        bounceTime: 300,
-        swipeTime: 500
-      },
-      draging: false,
-      scrollEndTimer: null,
-      scrollX: null
-    }
-  },
-  computed: {
-    currentValue() {
-      return this.data[this.currentIndex]
-    }
-  },
-  methods: {
-    handClick(key) {
-      if (this.currentIndex === key) return;
-      this.currentIndex = key;
-      this.scrollX = -this.currentIndex * this.itemWidth;
-      this.BScoll.scrollTo(this.scrollX, 0, 150);
-      this.$emit('input', this.currentValue);
+      default: 0,
     },
-    setCurrentIndex(x) {
-      let scrollNewX = Math.abs(x) + this.itemWidth / 2;
-      this.currentIndex = Math.floor(scrollNewX / this.itemWidth);
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    let bs = null;
+    const wgWrapper = ref(),
+      currentIndex = ref(0),
+      draging = ref(false),
+      scrollEndTimer = ref(0),
+      scrollX = ref(0);
 
-      console.log(this.currentIndex * this.itemWidth);
+    const currentValue = computed(() => props.data[currentIndex.value]);
 
-      this.scrollX = -this.currentIndex * this.itemWidth;
-
-      this.BScoll.scrollTo(this.scrollX, 0, 200);
-
-      this.$emit('input', this.currentValue);
-    },
-    touchEnd(pos) {
-      this.draging = false;
+    const init = () => {
+      bs = new BScroll(wgWrapper.value, {
+        scrollX: true,
+        scrollY: false,
+        observeDOM: true,
+        click: true,
+        probeType: 3,
+      });
+      bs.on('touchEnd', pos => touchEnd(pos));
+      bs.on('scrollEnd', pos => scrollEnd(pos));
+      bs.on('beforeScrollStart', () => beforeScrollStart());
+      bs.on('scrollCancel', () => scrollCancel());
+      setDefaultIndex();
+    };
+    const setCurrentIndex = x => {
+      const scrollNewX = Math.abs(x) + props.itemWidth / 2;
+      currentIndex.value = Math.floor(scrollNewX / props.itemWidth);
+      scrollX.value = -currentIndex.value * props.itemWidth;
+      bs.scrollTo(scrollX.value, 0, 100);
+      emit('update:modelValue', currentValue.value);
+    };
+    const touchEnd = pos => {
+      draging.value = false;
       console.log('鼠标/手指离开', pos);
-    },
-    scrollEnd(pos) {
-      clearTimeout(this.scrollEndTimer);
-      this.scrollEndTimer = setTimeout(() => {
-        if (this.draging || this.scrollX === pos.x) return false;
+    };
+    const scrollEnd = pos => {
+      clearTimeout(scrollEndTimer.value);
+      scrollEndTimer.value = setTimeout(() => {
+        if (draging.value || scrollX.value === pos.x) return false;
         console.log('滑动结束', pos);
-        this.draging = false;
-        this.setCurrentIndex(pos.x);
+        draging.value = false;
+        setCurrentIndex(pos.x);
       }, 50);
-    },
-    beforeScrollStart() {
-      this.draging = true;
+    };
+    const beforeScrollStart = () => {
+      draging.value = true;
       console.log('滑动开始之前');
-    },
-    scrollCancel() {
-      this.draging = false;
+    };
+    const scrollCancel = () => {
+      draging.value = false;
       console.log('滑动被取消');
-    },
-    setDefaultIndex() {
-      if (this.defaultIndex > 0) {
-        this.currentIndex = this.defaultIndex;
-        this.BScoll.scrollTo(-this.currentIndex * this.itemWidth, 0);
-        this.$emit('input', this.currentValue);
+    };
+
+    const setDefaultIndex = () => {
+      if (props.defaultIndex > 0) {
+        currentIndex.value = props.defaultIndex;
+        bs.scrollTo(-currentIndex.value * props.itemWidth, 0);
+        emit('update:modelValue', currentValue.value);
       }
-    }
+    };
+
+    const handClick = key => {
+      if (currentIndex.value === key) return;
+      currentIndex.value = key;
+      scrollX.value = -currentIndex.value * props.itemWidth;
+      bs.scrollTo(scrollX.value, 0, 150);
+      emit('update:modelValue', currentValue.value);
+    };
+    const activeItem = item => ({
+      width: props.itemWidth + 'px',
+      ...(item === currentValue.value ? props.pickerStyle : {}),
+    });
+
+    onMounted(async () => {
+      await nextTick();
+      init();
+    });
+    return {
+      wgWrapper,
+      activeItem,
+      handClick,
+    };
   },
-  mounted() {
-    this.$nextTick(function () {
-      this.BScoll = this.$refs.scroll.scroll;
-
-      this.BScoll.on('touchEnd', pos => this.touchEnd(pos))
-
-      this.BScoll.on('scrollEnd', pos => this.scrollEnd(pos))
-
-      this.BScoll.on('beforeScrollStart', () => this.beforeScrollStart())
-
-      this.BScoll.on('scrollCancel', () => this.scrollCancel())
-
-      this.setDefaultIndex()
-    })
-  }
-}
+});
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-.horizontal-scroll-list-wrap {
+<style lang="stylus">
+.horizontal-container {
+  position: relative;
   border: 1px solid rgba(0, 0, 0, 0.1);
-  height: 36px;
   border-radius: 20px;
+  height: 36px;
+  line-height: 36px;
+  overflow: hidden;
+  margin: 0 auto;
 
-  .cube-scroll-content {
+  .scroll-wrapper {
+    position: relative;
     display: inline-block;
-  }
+    z-index: 1;
 
-  .picker-slot-wrapper {
-    white-space: nowrap;
-    height: 36px;
-    line-height: 36px;
-    margin: 0;
-    padding: 0;
+    .scroll-content {
+      height: 36px;
+      line-height: 36px;
+      white-space: nowrap;
+      margin: 0;
+      padding: 0;
+    }
 
-    .picker-item {
+    .scroll-item {
       height: 100%;
       text-align: center;
       display: inline-block;
