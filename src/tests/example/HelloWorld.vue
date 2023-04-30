ad38399c0f8506618528cbfe2938077673a0a619
vincentzyc <vincentzyc@qq.com>
2023-04-30 12:21:02 +0800

    ✅test: unitest

---
+++ src/tests/example/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <span class="count">{{ count }}</span>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
