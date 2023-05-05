0ab280af5e2d8234950ad4e73646507336d19aca
vincentzyc <vincentzyc@qq.com>
2023-05-05 15:11:30 +0800

    🐳chore: vue2.7

--- src/components/widget-items/wg-count-down.vue
+++ src/components/widget-items/wg-count-down.vue
@@ -1,39 +1,40 @@
 <template>
   <div :style="$util.formatStyle(getStyle)" class="wg-countdown" v-if="!isFinished">
     <p class="flex flex-center">
-      <span class="countdown-title">{{item.title}}</span>
+      <span class="countdown-title">{{ item.title }}</span>
       <CountDown :time="endTime" @finish="finished" class="flex-inline align-middle">
         <template #default="timeData">
+          <span :style="$util.formatStyle(item.timeStyle)" class="countdown-timeblock" v-if="timeData.days > 0">{{
+            timeData.days
+          }}</span>
+          <span :style="$util.formatStyle(item.unitStyle)" class="countdown-colon" v-if="timeData.days > 0">天</span>
           <span
             :style="$util.formatStyle(item.timeStyle)"
             class="countdown-timeblock"
-            v-if="timeData.days>0"
-          >{{ timeData.days }}</span>
-          <span :style="$util.formatStyle(item.unitStyle)" class="countdown-colon" v-if="timeData.days>0">天</span>
-          <span
-            :style="$util.formatStyle(item.timeStyle)"
-            class="countdown-timeblock"
-            v-if="timeData.days>0||timeData.hours>0"
-          >{{ timeData.hours>9?timeData.hours:'0'+timeData.hours }}</span>
+            v-if="timeData.days > 0 || timeData.hours > 0"
+            >{{ timeData.hours > 9 ? timeData.hours : '0' + timeData.hours }}</span
+          >
           <span
             :style="$util.formatStyle(item.unitStyle)"
             class="countdown-colon"
-            v-if="timeData.days>0||timeData.hours>0"
-          >时</span>
+            v-if="timeData.days > 0 || timeData.hours > 0"
+            >时</span
+          >
           <span
             :style="$util.formatStyle(item.timeStyle)"
             class="countdown-timeblock"
-            v-if="timeData.days>0||timeData.hours>0||timeData.minutes>0"
-          >{{ timeData.minutes>9?timeData.minutes:'0'+timeData.minutes }}</span>
+            v-if="timeData.days > 0 || timeData.hours > 0 || timeData.minutes > 0"
+            >{{ timeData.minutes > 9 ? timeData.minutes : '0' + timeData.minutes }}</span
+          >
           <span
             :style="$util.formatStyle(item.unitStyle)"
             class="countdown-colon"
-            v-if="timeData.days>0||timeData.hours>0||timeData.minutes>0"
-          >分</span>
-          <span
-            :style="$util.formatStyle(item.timeStyle)"
-            class="countdown-timeblock"
-          >{{ timeData.seconds>9?timeData.seconds:'0'+timeData.seconds }}</span>
+            v-if="timeData.days > 0 || timeData.hours > 0 || timeData.minutes > 0"
+            >分</span
+          >
+          <span :style="$util.formatStyle(item.timeStyle)" class="countdown-timeblock">{{
+            timeData.seconds > 9 ? timeData.seconds : '0' + timeData.seconds
+          }}</span>
           <span :style="$util.formatStyle(item.unitStyle)" class="countdown-colon">秒</span>
         </template>
       </CountDown>
@@ -41,58 +42,47 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { CountDown } from 'vant';
-export default {
-  components: {
-    CountDown
+import { computed, nextTick, ref, onMounted } from 'vue';
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
   },
-  props: {
-    item: {
-      type: Object,
-      required: true
-    }
-  },
-  data() {
+});
+const isFinished = ref(false);
+const getStyle = computed(() => {
+  if (props.item.style.backgroundImage) {
     return {
-      isFinished: false
-    }
-  },
-  computed: {
-    getStyle() {
-      if (this.item.style.backgroundImage) {
-        return {
-          ...this.item.style,
-          backgroundImage: `url(${this.item.style.backgroundImage})`,
-          backgroundColor: 'transparent'
-        }
-      } else {
-        return this.item.style
-      }
-    },
-    endTime() {
-      return this.item.endTime - Date.now()
-    }
-  },
-  methods: {
-    finished() {
-      this.initCountDown()
-    },
-    getDayLoopEndTime() {
-      const today = new Date().toLocaleDateString()
-      const todayTimeStamp = new Date(today).getTime()
-      return todayTimeStamp + 24 * 60 * 60 * 1000
-    },
-    initCountDown() {
-      if (this.item.countDownType === 'dayLoop') {
-        this.item.endTime = this.getDayLoopEndTime()
-      } else {
-        this.isFinished = this.item.endTime < Date.now()
-      }
-    }
-  },
-  mounted() {
-    this.$nextTick(() => this.initCountDown())
+      ...props.item.style,
+      backgroundImage: `url(${props.item.style.backgroundImage})`,
+      backgroundColor: 'transparent',
+    };
+  } else {
+    return props.item.style;
+  }
+});
+const endTime = computed(() => {
+  return props.item.endTime - Date.now();
+});
+function finished() {
+  initCountDown();
+}
+function getDayLoopEndTime() {
+  const today = new Date().toLocaleDateString();
+  const todayTimeStamp = new Date(today).getTime();
+  return todayTimeStamp + 24 * 60 * 60 * 1000;
+}
+function initCountDown() {
+  if (props.item.countDownType === 'dayLoop') {
+    props.item.endTime = getDayLoopEndTime();
+  } else {
+    isFinished.value = props.item.endTime < Date.now();
   }
 }
+
+onMounted(() => {
+  nextTick(() => initCountDown());
+});
 </script>
